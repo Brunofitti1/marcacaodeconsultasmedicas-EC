c1ce684b9e92d7d74f5325697e83025940d09a81
Felipe Pereira de Assis <pereiradeassis03@gmail.com>
2025-03-28 20:22:13 -0300

    Inicialização do projeto com Expo e TypeScript - Versão 1.0

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.texto}>PEDRO LOPES DOMINGUES</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#777',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto: {
+    fontSize: 30,
+    color: '#0000FF',
+    alignItems: 'center',
+    fontWeight: 'bold'
+  }
 });
